6468b71a98c31bd5752d4eef8a1f5294e485ca79
Ashik-Faysal <ashikfaysal.rc@gmail.com>
2023-10-04 18:52:04 +0600

    added ashik Faysal

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ const Home = () => {
       <p>If you seen this and run successfully then send a  message to messenger group by writing : </p>
       <p><span tw='text-teal-700 font-bold'>WELCOME TO NEXT WORLD</span></p>
       <p tw='flex justify-center items-center mt-20 text-5xl text-cyan-600'>WELCOME GUYSSSSS..............</p>
+      <p>This is Ashik Faysal</p>
     </div>
   )
 }
